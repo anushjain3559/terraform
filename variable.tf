cdb339f385e960f9f4766b10248e8df131e08435
Anush Jain <111867498+anushjain3559@users.noreply.github.com>
2022-10-24 16:26:06 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,28 @@
+variable "env" {
+  type    = string
+  default = "DPC"
+}
+
+variable "aws_cidr" {
+  type    = string
+  default = "172.17.0.0/16"
+}
+
+variable "az_count" {
+  description = "Number of AZs to cover in a given AWS region"
+  default     = "2"
+}
+
+variable "key_pair" {
+  default = "congo"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.medium"
+}
+
+variable "passwd" {
+  type = string
+  default = "201cj8iuqsm0himje8zjezatt"
+}
